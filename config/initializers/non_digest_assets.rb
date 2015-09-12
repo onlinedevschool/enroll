f4c6cf949c7daddc6596d2b6ecfe6e4b645a650b
Jim OKelly <thatrubylove@gmail.com>
2015-09-11 22:35:26 -0500

    non digest on graphs

--- config/initializers/non_digest_assets.rb
+++ config/initializers/non_digest_assets.rb
@@ -1,1 +1,5 @@
-NonStupidDigestAssets.whitelist += ["ods-lg-sq.png"]
+NonStupidDigestAssets.whitelist += ["ods-lg-sq.png",
+                                    "graphs/time-investment.png",
+                                    "graphs/time-intensity.png",
+                                    "graphs/cost-per-hour.png",
+                                    "graphs/total-cost.png"]
