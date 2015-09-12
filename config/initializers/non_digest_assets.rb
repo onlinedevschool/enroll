dee6870fb07e843b69d0e736b499df59e85c0c31
Jim OKelly <thatrubylove@gmail.com>
2015-09-12 01:01:30 -0500

    added intensity

--- config/initializers/non_digest_assets.rb
+++ config/initializers/non_digest_assets.rb
@@ -2,4 +2,5 @@ NonStupidDigestAssets.whitelist += ["ods-lg-sq.png",
                                     "graphs/time-investment.png",
                                     "graphs/time-intensity.png",
                                     "graphs/cost-per-hour.png",
-                                    "graphs/total-cost.png"]
+                                    "graphs/total-cost.png",
+                                    "graphs/intensities.png"]
