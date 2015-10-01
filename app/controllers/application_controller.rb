0a96aa222225159cb39dd34ad2f029b97dead2f9
Jim OKelly <thatrubylove@gmail.com>
2015-09-30 19:43:06 -0500

    update site info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,16 +11,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def set_site_social_vars
-    @site_name = "ODS - OnlineDevSchool"
-    @site_title = "Welcome to the OnlineDevSchool (ODS) website. Learn to code, change your life!"
+    @site_brand = "devschool"
+    @site_name = "#{@site_brand} - afford a better life!"
+    @site_title = "Welcome to the #{@site_brand} website. Learn to code, change your life!"
     @site_url = "https://onlinedevschool.com"
     @site_share_image = "#{@site_url}/assets/ods-lg-sq.png"
     @site_description = <<-DESCRIPTION.strip_heredoc
-    The OnlineDevSchool has one mission. To teach you the skills you need
+    The #{@site_brand} has one mission. To teach you the skills you need
     to get a job as a technology maker, a web developer, in today's market.
     You are our primary concern and our 1 on 1 education is much more
     effective and personal than jamming 25 students together into a cohort.
     DESCRIPTION
-    @site_last_updated = 5.days.ago
+    @site_last_updated = 2.days.ago
   end
 end
+
