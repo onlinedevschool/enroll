e7e6430c1aefbec52215960e98f1ef6bd3fd35ac
Jim OKelly <thatrubylove@gmail.com>
2015-11-17 14:56:56 -0600

    fixed shit at the bottom of home

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151114234247) do
+ActiveRecord::Schema.define(version: 20151117215306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -112,20 +112,20 @@ ActiveRecord::Schema.define(version: 20151114234247) do
 
   create_table "posts", force: :cascade do |t|
     t.integer  "author_id"
-    t.string   "title",                    null: false
-    t.string   "permalink"
-    t.text     "html",                     null: false
-    t.text     "markdown",                 null: false
+    t.string   "title",                        null: false
+    t.string   "permalink",                    null: false
+    t.text     "html",                         null: false
+    t.text     "markdown",                     null: false
     t.datetime "published_at"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.integer  "category_id",  default: 1, null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.integer  "category_id",                  null: false
     t.integer  "series_id"
+    t.boolean  "internal",     default: false
   end
 
   add_index "posts", ["author_id"], name: "index_posts_on_author_id", using: :btree
   add_index "posts", ["category_id"], name: "index_posts_on_category_id", using: :btree
-  add_index "posts", ["permalink"], name: "index_posts_on_permalink", using: :btree
   add_index "posts", ["series_id"], name: "index_posts_on_series_id", using: :btree
 
   create_table "private_sessions", force: :cascade do |t|
