663c8af2fe464317194367bd4ff187f0240a0bf6
Jim OKelly <thatrubylove@gmail.com>
2015-12-19 16:54:54 -0600

    i am accptance

--- app/mailers/acceptance_team_mailer.rb
+++ app/mailers/acceptance_team_mailer.rb
@@ -3,7 +3,7 @@ class AcceptanceTeamMailer < ApplicationMailer
   def notify(enrollment)
     @enrollment = enrollment
     mail(
-      to: "applications@devschool.rocks",
+      to: "jim@devschool.rocks",
       subject: "A new application has been submitted!",
     )
   end
